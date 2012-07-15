df2954b49bf095c513e6932b8a222eded712089c
bad <drasky@ubuntu.(none)>
2012-07-15 03:07:12 -0300

    Adding gpl3 license to the sudoku_checker

--- CS258_Software_Testing/sudoku_checker.py
+++ CS258_Software_Testing/sudoku_checker.py
@@ -100,6 +100,28 @@ hard = [[1,0,0,0,0,7,0,9,0],
         [0,4,0,0,0,0,0,0,7],
         [0,0,7,0,0,0,3,0,0]]
 
+#
+# GPL LICENSE
+# -----------
+#
+# Copyright 2012 Alejandro Kondrasky <drasky.vanderhoff@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+
 
 def valid_range(l):
     val_elems = set(range(1, 10))
